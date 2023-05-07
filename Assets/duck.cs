a60f8f59ca665dab8d92b59734d6d670aa866daa
Enisa Dwi Lestari <enisadwi63368@gmail.com>
2023-05-07 17:57:25 +0700

    panel score and add script game over text

--- Assets/duck.cs
+++ Assets/duck.cs
@@ -97,7 +97,7 @@ public class duck : MonoBehaviour
 
         transform.DOScaleY(endValue: 0.1f, duration: 0.2f);
         isDie = true;
-        Invoke("Die", 3);
+        Invoke("Die", 1);
         }
 
         else if (other.CompareTag(tag: "Coin"))
