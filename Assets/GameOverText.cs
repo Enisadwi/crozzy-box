b5dddfb9c1fc5e8da377d1466d7f094a4446656d
Enisa Dwi Lestari <enisadwi63368@gmail.com>
2023-05-07 20:04:08 +0700

    countdown

--- Assets/GameOverText.cs
+++ Assets/GameOverText.cs
@@ -1,18 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class GameOverText : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] TMP_Text gameOverText;
 
-    // Update is called once per frame
-    void Update()
+    public void UpdateText(int score)
     {
-        
+        gameOverText.text = "Game Over!\n Your Score: "+ score;
     }
 }
